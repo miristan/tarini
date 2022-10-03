69cb0d7a69c17ab3a2c7c92d614d9f68f4cc15cf
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2022-10-03 17:11:05 +0530

    Project Tarini Created

---
+++ src/main/java/com/miristan/tarini/TariniApplication.java
@@ -0,0 +1,13 @@
+package com.miristan.tarini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TariniApplication.class, args);
+    }
+
+}
